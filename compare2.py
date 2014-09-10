be7784827a1f068d1a4d5800a099dfcf0e1395b9
vlad <vlad@affectv.co.uk>
2014-09-10 10:11:16 +0100

    ***

--- compare2.py
+++ compare2.py
@@ -1,6 +1,6 @@
+import Image
 import numpy
 import sys
-import PIL.Image as Image
 import time
 import contextlib
 from selenium import webdriver
@@ -11,10 +11,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
-t0=time.clock()
 
-desired_cap = {'os': 'Windows', 'os_version': '8', 'browser': 'chrome', 'browser_version': '31.0'}
-
+desired_cap = {'os': 'Windows', 'os_version': '8', 'browser': 'chrome', 'browser_version': '31.0' }
 
 driver = webdriver.Remote(
     command_executor='http://arslpnpi1:ngUasHjNZDJVHTAxXX8Y@hub.browserstack.com:80/wd/hub',
@@ -27,30 +25,23 @@ time.sleep(5)
 driver.get_screenshot_as_file('/home/vlad/PycharmProjects/img_compare/screenshotimage.png')
 driver.quit()
 
+
+
+
 def main():
     t1=time.clock()
     img1 = Image.open('screenshotimage2.png')
     img2 = Image.open('screenshotimage.png')
-    img1.crop((0,0,160,600)).save('pscreen.png')
-    img2.crop((0,0,160,600)).save('pscreen2.png')
-    im1 = Image.open('pscreen.png')
-    im2 = Image.open('pscreen2.png')
-
-
-    if im1.size != im2.size or im1.getbands() != im2.getbands():
+    if img1.size != img2.size or img1.getbands() != img2.getbands():
         return -1
 
     s = 0
-    # raise Exception('exeption')
     for band_index, band in enumerate(img1.getbands()):
-        m1 = numpy.array([p[band_index] for p in im1.getdata()]).reshape(*im1.size)
-        m2 = numpy.array([p[band_index] for p in im2.getdata()]).reshape(*im2.size)
+        m1 = numpy.array([p[band_index] for p in img1.getdata()]).reshape(*img1.size)
+        m2 = numpy.array([p[band_index] for p in img2.getdata()]).reshape(*img2.size)
         s += numpy.sum(numpy.abs(m1-m2))
     print s
-    if s>25000:
-        raise Exception('different')
-    print(time.clock()-t1)
-    print(time.clock()-t0)
+    print(time.clock()-t1 )
 
 if __name__ == "__main__":
     sys.exit(main())
